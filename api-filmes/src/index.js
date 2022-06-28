2aec5b70fe53ed98eb7f4f819c8b51c97f4be512
matheussilvatorres <matheustorres201900@gmail.com>
2022-06-27 21:34:05 -0300

    Update index.js

--- api-filmes/src/index.js
+++ api-filmes/src/index.js
@@ -10,7 +10,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/details:id' element={<Details/>}/>
+      <Route path='/details/:id' element={<Details/>}/>
     </Routes>
   </BrowserRouter>
 );
